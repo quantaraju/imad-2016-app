21ea8481da8f637b03404f87e585c0498df843ef
Narasimha Raju Nagubhai <narasimha.raju@gmail.com>
2016-09-28 10:27:52 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -39,8 +39,6 @@ button.onclick = function() {
 };
 
 // Submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
   // Make a request to the server and send the name
@@ -65,6 +63,8 @@ submit.onclick = function() {
 		}
 	};
 	// Make the request
+	var nameInput = document.getElementById('name');
+    var name = nameInput.value;
 	request.open('GET', 'http://quantaraju.imad.hasura-app.io/submit-name?name=' + name, true);
 	//request.open('GET', 'http://localhost:8080/counter', true);
 	request.send(null);  
